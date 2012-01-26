f7a87cbdd79c7b367dbaea1cf81f1a71ca1f598a
Oliver Nightingale <olivernightingale@Olivers-MacBook-Pro.local>
2012-01-26 14:17:02 +0000

    some nicer styles etc for the modal window

--- javascripts/views/film_view.js
+++ javascripts/views/film_view.js
@@ -9,9 +9,12 @@ define(['jquery', './../vendor/poirot', './../models/film', './../lib/jquery.dra
 
   FilmView.prototype = {
     render: function () {
+      var attributes = this.film.attributes
+      attributes.story_class = this.film.attr('story').toLowerCase().replace(/\s/g, '-').replace('&', 'and')
       this.html = poirot.filmView(this.film.attributes)
       this.html
         .delegate('form', 'change', this.findSimilarFilms.bind(this))
+        .delegate('section header', 'click', this.activateSection.bind(this))
         .draggable({dragHandle: 'header'})
 
       this.container.html(this.html)
@@ -20,6 +23,13 @@ define(['jquery', './../vendor/poirot', './../models/film', './../lib/jquery.dra
     findSimilarFilms: function (e) {
       var fieldValues = this.html.find('form').serializeArray().map(function (field) { return field.value })
       Film.findSimilarTo(this.film, fieldValues)
+    },
+
+    activateSection: function (e) {
+      e.preventDefault()
+      var header = $(e.target)
+      this.html.find('.active').removeClass('active')
+      header.parents('section').addClass('active')
     }
   }
 
