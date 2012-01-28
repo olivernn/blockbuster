8c90f9e828c263c53555207c4876c2343dc21050
Oliver Nightingale <olivernightingale@Olivers-MacBook-Pro.local>
2012-01-28 12:29:33 +0000

    more styling for the axis labels

--- javascripts/views/axis_label_view.js
+++ javascripts/views/axis_label_view.js
@@ -1,35 +1,42 @@
 define(['jquery', './../vendor/poirot'], function ($, poirot) {
 
   var AxisLabelView = function (orientation, axis) {
-    console.log('#' + orientation + '-axis-label-container')
     this.container = $('#' + orientation + '-axis-label-container')
     this.orientation = orientation
     this.axis = axis[orientation]
+    this.listDisplayed = false
   }
 
   AxisLabelView.prototype = {
     render: function () {
-      this.html = poirot.axisLabel($.extend({}, this.axis, {name: this.axis.name()}))
+      this.html = poirot.axisLabel($.extend({}, this.axis, {
+        name: this.axis.name(),
+        orientation: this.orientation
+      }))
 
       this.html
-        .delegate('h3', 'click', this.showList.bind(this))
+        .delegate('h3', 'click', this.toggleList.bind(this))
         .delegate('li', 'click', this.selectAxisLabel.bind(this))
 
       this.container.html(this.html)
-      console.log(this.container)
+    },
+
+    toggleList: function () {
+      this.listDisplayed ? this.hideList() : this.showList()
     },
 
     showList: function () {
+      this.listDisplayed = true
       this.html.find('ul').show()
     },
 
     hideList: function () {
+      this.listDisplayed = false
       this.html.find('ul').hide()
     },
 
     selectAxisLabel: function (e) {
       e.preventDefault()
-      console.log('foo')
       this.axis.name($(e.target).data('axis-label'))
     }
   }
