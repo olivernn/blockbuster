7e94e36570cc43b9e8a360d96d7baedccb7c9ac5
Oliver Nightingale <olivernightingale@Olivers-MacBook-Pro.local>
2012-02-03 23:58:04 +0000

    radar graph more complete

--- javascripts/views/radar_graph.js
+++ javascripts/views/radar_graph.js
@@ -1,31 +1,8 @@
-define(['jquery', 'lib/svg', 'models/film'], function ($, SVG, Film) {
-
-  var storyColor = {
-    'discovery': 'rgb(122,73,28)',
-    'escape': 'rgb(125, 0, 83)',
-    'journey and return': 'rgb(199,200,202)',
-    'comedy': 'rgb(34,164,151)',
-    'quest': 'rgb(144,200,133)',
-    'sacrifice': 'rgb(98,29,129)',
-    'wretched excess': 'rgb(89,90,93)',
-    'fish out of water': 'rgb(213,202,164)',
-    'maturation': 'rgb(204,56,33)',
-    'pursuit': 'rgb(161,71,141)',
-    'rivalry': 'rgb(66,171,72)',
-    'underdog': 'rgb(144,25,26)',
-    'love': 'rgb(144,200,133)',
-    'monster force': 'rgb(231,4,28)',
-    'revenge': 'rgb(112,183,50)',
-    'transformation': 'rgb(169,147,38)',
-    'metamorphosis': 'rgb(69,70,72)',
-    'rescue': 'rgb(231,50,128)',
-    'tragedy': 'rgb(177,114,33)',
-    'rags to riches': 'rgb(49,60,146)',
-    'the riddle': 'rgb(152,203,194)',
-    'temptation': 'rgb(211,147,126)'
-  }
+define(['jquery', 'lib/svg', 'models/film', 'lib/story_color'], function ($, SVG, Film, storyColor) {
 
   var elem, paper, group
+
+  var angleStep = (2 * Math.PI) / 5
 
   var prongs = [
     {
@@ -67,11 +44,23 @@ define(['jquery', 'lib/svg', 'models/film'], function ($, SVG, Film) {
   var draw = function (films) {
     polygons.empty()
     films.forEach(drawFilm)
+    drawAxis()
+  }
+
+  var drawAxis = function () {
+    prongs.forEach(function (prong, idx) {
+      var axis = new SVG.Line({stroke: '#111', 'stroke-width': 3})
+      axis.startPoint({x: 0, y: 0})
+      axis.endPoint(rotatePoint({x: 0, y: 75}, angleStep * idx))
+      axis.on('mouseover', function () {
+        console.log(prong.attrName)
+      })
+      polygons.add(axis)
+    })
   }
 
   var drawFilm = function (film) {
-    var polygon = new SVG.Polygon ({stroke: storyColor[film.normalizedStory()], fill: 'transparent'}),
-        angleStep = (2 * Math.PI) / 5
+    var polygon = new SVG.Polygon ({stroke: storyColor.forFilm(film), fill: 'transparent', 'stroke-width': 2})
 
     prongs.forEach(function (prong, idx) {
       var transformer = prong.transform || (function (n) { return n }),
@@ -79,6 +68,13 @@ define(['jquery', 'lib/svg', 'models/film'], function ($, SVG, Film) {
           point = rotatePoint({x: 0, y: value}, angleStep * idx)
 
       polygon.addPoint(point)
+    })
+
+    polygon.on('mouseover', function () {
+      film.highlight()
+    })
+    polygon.on('mouseout', function () {
+      setTimeout(film.unhighlight(), 600)
     })
 
     polygons.add(polygon)
